9493d9d0f34e25f33c0f6d6ffe8c0bc46f0eefba
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-09-03 12:41:00 +0100

    Update schemas

--- schemas.py
+++ schemas.py
@@ -1,15 +1,18 @@
-from pydantic import BaseModel, Field, EmailStr
-from uuid import UUID, uuid4
 from enum import IntEnum, Enum
+from typing import Optional
+from uuid import UUID, uuid4
+
+from pydantic import BaseModel, Field, EmailStr
+
 
 class Level(IntEnum):
-    L100=100
-    L200=200
-    L300=300
-    L400=400
-    L500=500
-    L600=600
-    L700=700
+    L100 = 100
+    L200 = 200
+    L300 = 300
+    L400 = 400
+    L500 = 500
+    L600 = 600
+    L700 = 700
 
 
 class AdmissionMode(str, Enum):
@@ -17,32 +20,71 @@ class AdmissionMode(str, Enum):
     DIRECT_ENTRY = "de"
 
 
-class ClassSchema(BaseModel):
+class BaseClassSchema(BaseModel):
     id: UUID = Field(default_factory=uuid4)
-    display_name: str
+    display_name: str | None
     level: Level
-    faculty: str
-    department: str
-    governor: "StudentSchema" | None
-    deputy: "StudentScheme" | None
+    department_id: UUID
 
-class StudentScheme(BaseModel):
-    id: Field(default_factory=uuid4)
+
+class CreateClassSchema(BaseClassSchema):
+    ...
+
+
+class UpdateClassSchema(BaseClassSchema):
+    governor_id: Optional[UUID]
+    deputy_id: Optional[UUID]
+
+
+class StudentSchema(BaseModel):
+    id: UUID = Field(default_factory=uuid4)
     class_id: UUID
     first_name: str
     middle_name: str
     last_name: str
     admission_mode: AdmissionMode
-    matric_no: Optional[str] # TODO: validate
-    reg_no: Optional[str] # TODO: validate
+    matriculation_number: str | None  # TODO: validate
+    jamb_registration_number: str | None  # TODO: validate
     personal_email_address: EmailStr
     school_email_address: EmailStr
+
 
 class PhoneNumberAvailability(str, Enum):
     CALLS = "CALLS"
     WHATSAPP = "WHATSAPP"
     TELEGRAM = "TELEGRAM"
 
+
 class PhoneNumberSchema(BaseModel):
-    number: str # TODO: validate
+    number: str  # TODO: validate
     availability: list[PhoneNumberAvailability]
+
+
+class CreateUpdateSchoolSchema(BaseModel):
+    name: str
+
+
+class SchoolSchema(BaseModel):
+    id: UUID
+    name: str
+
+
+class FacultySchema(BaseModel):
+    id: UUID
+    name: str
+    school_id: UUID
+
+
+class CreateFacultySchema(BaseModel):
+    name: str
+
+
+class DepartmentSchema(BaseModel):
+    id: UUID
+    name: str
+    faculty_id: UUID
+
+
+class ResponseSchema(BaseModel):
+    message: str | None
+    data: dict | list | None
