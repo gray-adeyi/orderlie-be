81148f6292e0b25afca0483ed115356d51740088
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-09-04 00:58:41 +0100

    Register school_router to app

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
-from routers import class_router, student_router, school_router
+from routers import school_router
 
 app = FastAPI(
     title="Orderlie API",
     description="Collect, organize and & export class biodata",
 )
-app.include_router(class_router)
-app.include_router(student_router)
-app.include_router(school_router)
+app.include_router(school_router, prefix="/api/v1")
 
 
 @app.get("/")
