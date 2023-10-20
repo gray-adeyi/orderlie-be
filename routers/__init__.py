ea3a314e0111e514983420d1e91f45f9af0b39e7
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-10-20 15:08:28 +0100

    Refactor redundant api nesting

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,2 +1,5 @@
 from .classes import class_router
+from .classes import class_router
+from .departments import department_router
+from .faculties import faculty_router
 from .schools import school_router
--- routers/faculties.py
+++ routers/faculties.py
@@ -7,7 +7,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from db import get_session_as_dependency
 from models import School, Faculty
-from routers.departments import department_router
 from schemas import (
     ResponseSchema,
     DepartmentSchema,
@@ -16,8 +15,7 @@ from schemas import (
 )
 from utils import get_model_by_id_or_404, get_one_model_obj_by_query_or_404
 
-faculty_router = APIRouter(prefix="/{school_id}/faculties", tags=["faculties"])
-faculty_router.include_router(department_router)
+faculty_router = APIRouter(prefix="/faculties", tags=["faculties"])
 
 
 @faculty_router.get("")
@@ -25,6 +23,12 @@ async def get_school_faculties(
     school_id: UUID,
     db: AsyncSession = Depends(get_session_as_dependency),
 ) -> ResponseSchema:
+    """
+    This endpoint lets you retrieve the faculties a school has by the school's unique identifier
+
+    Note:
+        Current implementation does not support pagination but will get included in future releases.
+    """
     school = cast(School, (await get_model_by_id_or_404(db, School, school_id)))
     faculties = []
     for faculty in await school.awaitable_attrs.faculties:
@@ -40,14 +44,21 @@ async def get_school_faculties(
 
 @faculty_router.post("", status_code=status.HTTP_201_CREATED)
 async def create_school_faculty(
-    school_id: UUID,
     faculty_data: CreateUpdateFacultySchema,
     db: AsyncSession = Depends(get_session_as_dependency),
 ) -> ResponseSchema:
-    school = cast(School, (await get_model_by_id_or_404(db, School, school_id)))
-    data = faculty_data.model_dump()
-    data["school_id"] = school_id
-    faculty = await Faculty.create(db=db, data=data)
+    """This endpoint let's you create a faculty for a school.
+
+    Note:
+        This endpoint is not for direct use to end users but available to Orderlie admins to create
+        and manage faculites created on the platform. This helps mitigate the creation of the
+        same faculties with different names.
+        This endpoint will be protected by authentication
+    """
+    school = cast(
+        School, (await get_model_by_id_or_404(db, School, faculty_data.school_id))
+    )
+    faculty = await Faculty.create(db=db, data=faculty_data.model_dump())
     faculty.__dict__["departments"] = await faculty.awaitable_attrs.departments
     return ResponseSchema(
         message="faculties successfully created",
@@ -57,15 +68,11 @@ async def create_school_faculty(
 
 @faculty_router.get("/{faculty_id}")
 async def get_school_faculty(
-    school_id: UUID,
     faculty_id: UUID,
     db: AsyncSession = Depends(get_session_as_dependency),
 ) -> ResponseSchema:
-    query = (
-        select(Faculty)
-        .where(Faculty.school_id == school_id)
-        .where(Faculty.id == faculty_id)
-    )
+    """This endpoint lets you retrieve a faculty by it's unique identifier"""
+    query = select(Faculty).where(Faculty.id == faculty_id)
     faculty = await get_one_model_obj_by_query_or_404(db=db, statement=query)
     return ResponseSchema(
         message="faculties successfully retrieved",
@@ -75,16 +82,19 @@ async def get_school_faculty(
 
 @faculty_router.patch("/{faculty_id}")
 async def update_school_faculty(
-    school_id: UUID,
     faculty_id: UUID,
     update_data: CreateUpdateFacultySchema,
     db: AsyncSession = Depends(get_session_as_dependency),
 ) -> ResponseSchema:
-    query = (
-        select(Faculty)
-        .where(Faculty.school_id == school_id)
-        .where(Faculty.id == faculty_id)
-    )
+    """This endpoint lets you update the information of an existing faculty.
+
+    Note:
+        This endpoint is not for direct use to end users but available to Orderlie admins to manage
+        faculties on the platform. This helps mitigate the creation of the
+        same faculties with different names.
+        This endpoint will be protected by authentication
+    """
+    query = select(Faculty).where(Faculty.id == faculty_id)
     faculty = cast(
         Faculty,
         (
